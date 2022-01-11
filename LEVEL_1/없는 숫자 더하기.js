989f35b21ef948661cb305a16ac1e34645dde6c0
시훈 <81404026+sihun96@users.noreply.github.com>
2022-01-11 22:41:29 +0900

    Add files via upload

---
+++ LEVEL_1/없는 숫자 더하기.js
@@ -0,0 +1,36 @@
+function solution20(numbers) {
+  let answer = 45;
+  for (let a = 0; a < numbers.length; a++) {
+    switch (numbers[a]) {
+      case 1:
+        answer -= 1;
+        break;
+      case 2:
+        answer -= 2;
+        break;
+      case 3:
+        answer -= 3;
+        break;
+      case 4:
+        answer -= 4;
+        break;
+      case 5:
+        answer -= 5;
+        break;
+      case 6:
+        answer -= 6;
+        break;
+      case 7:
+        answer -= 7;
+        break;
+      case 8:
+        answer -= 8;
+        break;
+      case 9:
+        answer -= 9;
+        break;
+    }
+  }
+  return answer;
+}
+console.log(solution20([5, 8, 4, 0, 6, 7, 9]));
