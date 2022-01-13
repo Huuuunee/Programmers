72a26e9217ee9d191c3833657143b31a6afc7129
시훈 <81404026+sihun96@users.noreply.github.com>
2022-01-13 20:59:27 +0900

    Add files via upload

---
+++ LEVEL_1/맵.js
@@ -0,0 +1,25 @@
+function solution1111(n, arr1, arr2) {
+  let answer = [];
+  let result = "";
+  let rrrr = [];
+  let padding = "000000";
+  for (let a = 0; a < n; a++) {
+    arr1[a] = arr1[a].toString(2).padStart(n, "0");
+    arr2[a] = arr2[a].toString(2).padStart(n, "0");
+    for (let b = 0; b < n; b++) {
+      if (
+        (arr1[a][b] == 1 && arr2[a][b] == 1) ||
+        (arr1[a][b] == 1 && arr2[a][b] == 0) ||
+        (arr1[a][b] == 0 && arr2[a][b] == 1)
+      ) {
+        answer[a] += "#";
+      } else if (arr1[a][b] == 0 && arr2[a][b] == 0) answer[a] += " ";
+    }
+    result = answer[a].replace("undefined", "");
+
+    rrrr[a] = result;
+  }
+
+  return rrrr;
+}
+console.log(solution1111(5, [9, 20, 28, 18, 11], [30, 1, 21, 17, 28]));
