93420ce676768da3a9629b840d7917476f0a117d
시훈 <81404026+Huuuunee@users.noreply.github.com>
2022-04-30 13:19:22 +0900

    Add files via upload

---
+++ LEVEL_1/약수의 개수와 합.js
@@ -0,0 +1,17 @@
+function solution(left, right) {
+    let answer = 0;
+    for(let i = left; i<=right;i++){
+        let count = 0;
+        for(let j = 2;j<=i;j++){
+            if(i % j == 0){
+                count += 1;
+            }
+        }
+        if(count % 2 == 0)
+            answer -= i;
+        else
+            answer += i;
+    }
+    return answer;
+}
+console.log(solution(24,27))
