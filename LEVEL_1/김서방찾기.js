4c31f084e64c87e7b42e1084f5b817cabc9a871c
시훈 <81404026+Huuuunee@users.noreply.github.com>
2022-08-03 23:31:54 +0900

    Update 김서방찾기.js

--- LEVEL_1/김서방찾기.js
+++ LEVEL_1/김서방찾기.js
@@ -1,11 +1,9 @@
-function solution18(seoul) {
+function solution(seoul) {
   let answer = "";
   seoul.map((a, b, c) => {
     if (a == "Kim") {
-      console.log(b);
       return (answer = b);
     }
   });
   return "김서방은 " + answer + "에 있다";
 }
-console.log(solution18(["Jane", "Kim"]));
